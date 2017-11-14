0100368563bb20134d1c98bfe5a86692f559efad
razil <razil0071999@gmail.com>
2017-11-14 13:27:31 +0300

    домашки
    домашки

---
+++ src/ru/itis/Main.java
@@ -0,0 +1,9 @@
+package ru.itis;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//int a[] = new int[Integer.MAX_VALUE];
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
