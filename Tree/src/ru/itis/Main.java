8583346ddfa444e0dfb69d78d4759222d994df41
razil <razil0071999@gmail.com>
2018-04-10 22:19:44 +0300

    add some methods in tree

--- Tree/src/ru/itis/Main.java
+++ Tree/src/ru/itis/Main.java
@@ -3,8 +3,8 @@ package ru.itis;
 public class Main {
 
     public static void main(String[] args) {
-        int array[] = {5, 4, 8, 2, 5, 6, 10, 1, 3, 6, 7, 4, 10};
-        int array1[] = {5, 4, 8, 2, 5, 6, 10, 1, 3, 6, 7, 4, 10};
+        int array[] = {5, 4, 8, 2, 5, 6, 10, 1, 3, 6, 7, 4};
+        int array1[] = {5, 4, 8, 2, 5, 6, 10, 1, 3, 6, 7, 4};
 
         TreeBstImpl<Integer> tree = new TreeBstImpl<>();
 
@@ -17,10 +17,13 @@ public class Main {
         for (int i = 0; i < array.length; i++) {
             tree1.insert(array1[i]);
         }
-
-
         tree.print();
-        System.out.println(tree1.equals(tree));
+        tree.remove(5);
+        System.out.println();
+        tree.print();
+
+        //tree.print();
+        //tree.printByLevels();
 
 //        Node a = new Node(5);
 //        Node b = new Node(4);
--- Tree/src/ru/itis/TreeBstImpl.java
+++ Tree/src/ru/itis/TreeBstImpl.java
@@ -1,5 +1,8 @@
 package ru.itis;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * 03.04.2018
  * TreeBstImpl
@@ -12,6 +15,7 @@ public class TreeBstImpl<T extends Comparable<T>> implements Tree<T> {
         private T value;
         private Node left;
         private Node right;
+        private Node parent;
 
         Node(T value) {
             this.value = value;
@@ -46,14 +50,53 @@ public class TreeBstImpl<T extends Comparable<T>> implements Tree<T> {
             root = new Node(value);
         } else if (value.compareTo(root.value) <= 0) {
             root.left = insert(root.left, value);
+            root.left.parent = root;
         } else {
             root.right = insert(root.right, value);
+            root.right.parent = root;
         }
         return root;
     }
 
     //TODO:
     public boolean remove(T value) {
+        Node node = find(value);
+        Node parent = node.parent;                                 // предок удаляемого элемента
+        if (node.left == null & node.right == null) {       // первый случай: удаляемый элемент - лист
+            if (parent.left == node)
+                parent.left = null;
+            if (parent.right == node)
+                parent.right = null;
+            return true;
+        } else if (node.left == null | node.right == null)     // второй случай: удаляемый элемент имеет одного потомка
+            if (node.left == null) {
+                if (parent.left == node)
+                    parent.left = node.right;
+                else
+                    parent.right = node.right;
+                node.right.parent = parent;
+            } else {
+                if (parent.left == node)
+                    parent.left = node.left;
+                else
+                    parent.right = node.left;
+                node.left.parent = parent;
+                return true;
+            }
+        else {                                        // третий случай: удаляемый элемент имеет двух потомков
+            Node successor = next(node);
+            node.value = successor.value;
+            if (successor.parent.left == successor) {
+                successor.parent.left = successor.right;
+                if (successor.right != null)
+                    successor.right.parent = successor.parent;
+            } else {
+                successor.parent.right = successor.right;
+                if (successor.right != null)
+                    successor.right.parent = successor.parent;
+            }
+            return true;
+        }
         return false;
     }
 
@@ -77,25 +120,87 @@ public class TreeBstImpl<T extends Comparable<T>> implements Tree<T> {
     }
 
 
-
-
-
     //TODO:
     @Override
     public void printByLevels() {
+        printByLevels(root);
 
     }
+
+    int capacity = 1;
+    LinkedList<ArrayList> list = new LinkedList();
+
+    private void printByLevels(Node node) {
+        ArrayList<T> arr = new ArrayList<>();
+        arr.add(node.value);
+        list.add(arr);
+        list.add(printEachLevel(node));
+        iDoNotKnowHowToNameThisMethod(root);
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.get(i).size(); j++)
+                System.out.print(list.get(i).get(j) + " ");
+            System.out.println();
+        }
+
+
+    }
+
+    private void iDoNotKnowHowToNameThisMethod(Node node) {
+        try {
+            list.add(merge(printEachLevel(node.left), printEachLevel(node.right)));
+            iDoNotKnowHowToNameThisMethod(node.right);
+            iDoNotKnowHowToNameThisMethod(node.left);
+        } catch (NullPointerException e) {
+
+        }
+    }
+
+    private ArrayList<T> merge(ArrayList<T> a, ArrayList<T> b) {
+        ArrayList<T> arrayList = new ArrayList();
+        for (int i = 0; i < a.size(); i++)
+            arrayList.add(a.get(i));
+        for (int i = 0; i < b.size(); i++)
+            arrayList.add(b.get(i));
+        return arrayList;
+    }
+
+    private ArrayList printEachLevel(Node node) {
+        ArrayList<T> arrayList = new ArrayList();
+        arrayList.add(node.left.value);
+        arrayList.add(node.right.value);
+        return arrayList;
+    }
+
+    public void printByLevels2(){
+        System.out.println(root.value);
+        printByLevels(root);
+
+    }
+    public void printByLevels2(Node node){
+        System.out.println(node.value);
+        printByLevels(node.);
+
+    }
+
+
 
     //TODO
     @Override
     public boolean isBst() {
-        return false;
+        return isBst(root);
     }
 
-    private boolean isBst(Node node){
-        if(node.left == null)
-        if (node.left.value.compareTo(node.value) <= 0);
-        return false;
+    private boolean isBst(Node node) {
+        if (node != null) {
+            if ((node.left != null && node.value.compareTo(node.left.value) < 0) || (node.right != null && node.value.compareTo(node.right.value) >= 0)) {
+                return false;
+            } else {
+                isBst(node.left);
+                isBst(node.right);
+            }
+        }
+        return true;
     }
 
 
@@ -116,4 +221,35 @@ public class TreeBstImpl<T extends Comparable<T>> implements Tree<T> {
             return false;
         }
     }
+
+    private Node find(T value) {
+        Node node = root;
+        while (node != null) {
+            if (node.value.compareTo(value) == 0)
+                return node;
+            if (node.value.compareTo(value) > 0)
+                node = node.left;
+            else node = node.right;
+        }
+        return null;
+    }
+
+    private Node next(Node node) {
+        if (node.right != null)
+            return minimum(node.right);
+        Node y = node.parent;
+        while (y != null & node == y.right) {
+            node = y;
+            y = y.parent;
+        }
+        return y;
+    }
+
+    private Node minimum(Node node) {
+        if (node.left == null)
+            return node;
+        return minimum(node.left);
+    }
+
+
 }
