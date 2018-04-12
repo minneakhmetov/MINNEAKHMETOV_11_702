a3e084561b8c9153064f0111ffb4d98703035bcc
razil <razil0071999@gmail.com>
2018-04-12 19:41:51 +0300

    add optimization

--- IncomprehensibleTree/src/ru/itis/Main.java
+++ IncomprehensibleTree/src/ru/itis/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	Tree<Integer> tree = new Tree<>();
-	Integer[] ints = {1, 3, 6, 8, 2, 5, 7, 9, 2, 5, 7};
+	Integer[] ints = {1, 3, 6, 8, 2, 5, 7, 9, 2, 5, 7, 6, 8, 8, 3, 5, 6, 7, 9, 1, 3, 4, 5};
 	tree.convert(ints, 3);
 	tree.print();
+	tree.squarePrint();
+
     }
 }
--- IncomprehensibleTree/src/ru/itis/Tree.java
+++ IncomprehensibleTree/src/ru/itis/Tree.java
@@ -3,42 +3,65 @@ package ru.itis;
 public class Tree<T extends Comparable> {
 
     Node root;
+    int sum = 0;
 
-    Tree (){
+    Tree() {
         this.root = new Node(0);
     }
+
     int position = 0;
 
-    public void convert(T[] array, int K){
+    public void convert(T[] array, int K) {
         convert(root, array, K);
     }
-    private void convert(Node node, T[] array, int K){
+
+    int k = -1;
+
+    private void convert(Node node, T[] array, int K) {
         try {
             node.nodes = new Node[K];
             for (int i = 0; i < K; i++) {
                 node.nodes[i] = new Node(array[position]);
+                node.nodes[i].parent = node;
                 position++;
             }
-            for (int i = 0; i < K; i++)
-                convert(node.nodes[i], array, K);
+            k++;
+            if (node == root) {
+                sum++;
+                convert(node.nodes[0], array, K);
+            } else if (k == K) {
+                k = 0; sum++;
+                convert(node.nodes[k], array, K);
+            } else {
+                convert(node.parent.nodes[k], array, K);
+                sum++;
+            }
+
         } catch (ArrayIndexOutOfBoundsException e) {
 
         }
     }
 
-    public void print(){
+    public void print() {
         print(root);
     }
-    private void print(Node node){
+
+    private void print(Node node) {
         try {
             for (int i = 0; i < node.nodes.length; i++)
-                System.out.print(node.nodes[i].value +  "Parent:" + node.value + "   ");
+                System.out.print(node.nodes[i].value + "Parent:" + node.value + "   ");
             System.out.println();
             for (int i = 0; i < node.nodes.length; i++)
                 print(node.nodes[i]);
-        } catch (NullPointerException e){
+        } catch (NullPointerException e) {
 
         }
+
+    }
+
+    public void squarePrint(){
+        System.out.println();
+        System.out.println("Количесвто квадратов " + sum);
     }
 
 }
